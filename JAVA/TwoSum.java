87dd2a44f11da7d5d570f6e504d9b3ec762c24e8
BlazePhenom <saxmax230505@gmail.com>
2024-10-31 02:08:35 +0530

    Update TwoSum.java

--- JAVA/TwoSum.java
+++ JAVA/TwoSum.java
@@ -1,11 +1,12 @@
 // A Program to solve the First Question of LEETCODE i.e TWOSUM
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] arr, int target) {// arr variables refers to the array that we have inputed.
+        //target variable is referring to the target sum for which we have to check 2 indexes of the array such that both the indexes be different.
         int[] ans = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
                     ans = new int[]{i,j};
                 }
             }
